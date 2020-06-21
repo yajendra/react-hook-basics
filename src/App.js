d9e280fad780fa0e78c364e6e0bda6469f32fca6
Yajendra <yajendra@truelancer.com>
2020-06-21 19:13:55 +0530

    Use Effect Concept

--- src/App.js
+++ src/App.js
@@ -1,26 +1,31 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+ 
+function App(){
+    const [data, setData] = useState({ hits: [] });
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    useEffect(() => {        
+        const fetchData = async () => {
+            const result = await axios(
+                'https://hn.algolia.com/api/v1/search?query=redux',
+            );
+ 
+            setData(result.data);
+        };
+ 
+        fetchData();
+    
+    },[]);
+
+    return(
+        <ul>
+            {data.hits.map(item => (
+                <li key={item.objectID}>
+                    <a href={item.url}>{item.title}</a>
+                </li>
+            ))}
+        </ul>
+    );
 }
-
+ 
 export default App;
