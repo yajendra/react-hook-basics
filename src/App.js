77c506d1e35b11de2cb560e8f987cea28c22d77e
Yajendra <yajendra@truelancer.com>
2020-06-21 23:52:18 +0530

    Reducer Concept

--- src/App.js
+++ src/App.js
@@ -1,31 +1,57 @@
-import React, { Fragment, useState, useEffect } from 'react';
+import React, { Fragment, useState, useEffect, useReducer } from 'react';
 import axios from 'axios';
 
+const dataFetchReducer = (state, action) => {
+    switch(action.type){
+        case 'FETCH_INIT':
+            return {
+                ...state,
+                isLoading: true,
+                isError: false,
+            }; 
+        break;
+        case 'FETCH_SUCCESS':
+            return {
+                ...state,
+                isLoading: false,
+                isError: false,
+                data:action.payload,
+            };
+        break;
+        case 'FETCH_FAILURE':
+            return {
+                ...state,
+                isLoading: false,
+                isError: true,
+            };
+        break;
+        default:
+            throw new Error();
+    }
+};
+
+
 const useHackerNewsApi = (initialUrl, initialData) => {
-    const [data, setData]   = useState(initialData);
-    const [url, setUrl]     = useState(initialUrl);
-    const [isLoading, setIsLoading] = useState(false);
-    const [isError, setIsError]     = useState(false);
+    const [url, setUrl]             = useState(initialUrl);
+    const [combinedstate, dispatch] = useReducer(dataFetchReducer,{isLoading :false,isError:false,data: initialData });
 
     useEffect(() => {
         const fetchData = async () => {
-            setIsError(false);
-            setIsLoading(true);
+            dispatch({ type: 'FETCH_INIT' });
 
             try{
                 const result = await axios(url);
-                setData(result.data);
+                dispatch({ type: 'FETCH_SUCCESS' , payload : result.data });
             }catch(error){
-                setIsError(true);
-            } 
+                dispatch({ type: 'FETCH_FAILURE' });
+            }            
             
-            setIsLoading(false);
         };
  
         fetchData();    
     },[url]);
 
-    return [{ data, isLoading, isError }, setUrl];
+    return [combinedstate, setUrl];
 }
  
 function App(){
