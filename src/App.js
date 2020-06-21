2720c50b82ed980ca81eb3e226d26ae3300b2ae7
Yajendra <yajendra@truelancer.com>
2020-06-21 19:34:36 +0530

    Optimize Code

--- src/App.js
+++ src/App.js
@@ -2,22 +2,20 @@ import React, { Fragment, useState, useEffect } from 'react';
 import axios from 'axios';
  
 function App(){
-    const [data, setData] = useState({ hits: [] });
+    const [data, setData]   = useState({ hits: [] });
     const [query, setQuery] = useState('redux');
-    const [search, setSearch] = useState('redux');
+    const [url, setUrl]     = useState('https://hn.algolia.com/api/v1/search?query=redux');
 
     useEffect(() => {        
         const fetchData = async () => {
-            const result = await axios(
-                `http://hn.algolia.com/api/v1/search?query=${search}`,
-            );
+            const result = await axios(url);
  
             setData(result.data);
         };
  
         fetchData();
     
-    },[search]);
+    },[url]);
 
     return(
         <Fragment>
@@ -26,7 +24,7 @@ function App(){
                 value={query}
                 onChange={event => setQuery(event.target.value)}
             />
-            <button type="button" onClick={() => setSearch(query)}>
+            <button type="button" onClick={() => setUrl(`http://hn.algolia.com/api/v1/search?query=${query}`) }>
                 Search
             </button>
             <ul>
